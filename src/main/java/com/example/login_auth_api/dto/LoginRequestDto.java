0c8a571ddf254c76e14479f8f376181a676e561b
pedro-batista <peddrobatista2022@gmail.com>
2025-05-17 19:11:32 -0300

    Primeiro commit...

---
+++ src/main/java/com/example/login_auth_api/dto/LoginRequestDto.java
@@ -0,0 +1,4 @@
+package com.example.login_auth_api.dto;
+
+public record LoginRequestDto(String email, String password) {
+}
---
+++ src/main/java/com/example/login_auth_api/dto/ResponseDto.java
@@ -0,0 +1,4 @@
+package com.example.login_auth_api.dto;
+
+public record ResponseDto(String name, String token) {
+}
